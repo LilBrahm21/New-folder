3a86bd0b3795c2e065bd877d3ad197cef7373d93
Brahm <brahm.brar@ucas-edu.net>
2025-02-11 15:43:09 +0000

    variables_practice.py

---
+++ Variables/variables_practice.py
@@ -0,0 +1,20 @@
+name = "brahm"
+lowNumber = "9"
+highNumber = "109"
+breakfast = "cereal"
+favoriteColor = "red"
+school = "UCAS"
+year = "2025"
+eyeColor = "brown"
+age = "14"
+favoriteSubject = "PE"
+print(name)
+print(lowNumber)
+print(highNumber)
+print(breakfast)
+print(favoriteColor)
+print(school)
+print(year)
+print(eyeColor)
+print(age)
+print(favoriteSubject)
