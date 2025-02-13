f41df91b430da47f4d2ad80125f1145eb2defbcd
Brahm <brahm.brar@ucas-edu.net>
2025-02-13 04:20:05 +0000

    firstproject.c

---
+++ Variables/first_project.c
@@ -0,0 +1,10 @@
+//brahm brar first project C
+#include<stdio.h>
+
+int main(void){
+    char name[30];
+    printf("What is your full name?; \n");
+    scanf("%s", name);
+    printf("Hello %s", name);
+    return 0;
+}
