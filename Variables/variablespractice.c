4657f1447b69ae0ceee8196bb91b0cfab9f0a4c7
Brahm <brahm.brar@ucas-edu.net>
2025-02-13 04:37:42 +0000

    variablespractice.c

---
+++ Variables/variablespractice.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void){
+    char name[] = "Brahm Brar";
+    printf("%s\n", name);
+    char lowNumber[] = "10 ";
+    printf("%s\n", lowNumber );
+    char highNumber[] = "110";
+    printf("%s\n", highNumber);
+    char breakfast[] = "cereal";
+    printf("%s\n", breakfast);
+    char favoriteColor[] = "red";
+    printf("%s\n", favoriteColor);
+    char school[] = "UCAS";
+    printf("%s\n", school);
+    char year[] = "2025";
+    printf("%s\n", year); 
+    char eyeColor[] = "brown";
+    printf("%s\n", eyeColor);
+    char age[] = "14";
+    printf("%s\n", age);
+    char favoriteSubject[] = "math";
+    printf("%s\n", favoriteSubject);
+    return 0;
+}
