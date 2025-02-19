f6308a75ee249f61abff1efe3e738013e9772be8
LilBrahm21 <brahm.brar@ucas-edu.net>
2025-02-19 11:48:52 -0700

    notes.c

--- functions/notes.C
+++ functions/notes.C
@@ -1,4 +1,11 @@
 //brahm brar, function notes in C
 #include <stdio.h>
 int num;
-char
+char name[50],verb[50];
+float add(int numOne, int numTwo){
+    return numOne + numTwo;
+}
+float due(char assignment[50], char day [20]){
+    printf("The %s assignment is due %s", assignment, day)
+}
+
