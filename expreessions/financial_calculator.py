e93cb210db0485cbdd6124d7ddb757f83c302f87
LilBrahm21 <brahm.brar@ucas-edu.net>
2025-02-21 11:49:02 -0700

    financial calculator.py

--- expreessions/financial_calculator.py
+++ expreessions/financial_calculator.py
@@ -17,23 +17,25 @@ saving= income*.1
 # calculate spending as income-savings-rent-utilites-groceries-transportation (variable)
 spending = income-rent-utilities-groceries-transportation
 # calculate percent income of rent (income/income*100) (variable)
-rent_percentage = rent/income *100
+rent_percentage = rent//income *100
 # calculate percent income of utilites (income/income*100) (variable)
-utilities = utilities/income *100
+utilities_percentage = utilities//income *100
 # calculate percent income of groceries (income/income*100) (variable)
-groceries = groceries/income *100
+groceries_percentage = groceries//income *100
 # calculate percent income of transportation (income/income*100) (variable)
-transportation = transportation/income *100
+transportation_percentage = transportation//income *100
 # Your rent is $X.XX which is XX% of your income. (print)
-spending= spending/income *100
+spending_percentage = spending//income *100
+saving_percentage = saving//income *100
 # Your utilites is $XX.XX which is XX% of your income. (print)
-print("your rent is $", income, "which is" ,rent_percentage, "of your income")
+print("your rent is $", rent, "which is" ,rent_percentage, "of your income")
 # Your groceries is $XX.XX which is XX% of your income. (print)
 print("your utilities is $", utilities, " ,which is" , utilities_percentage, "of your income")
 # Your transportation is $XX.XX which is XX% of your income. (print)
-print("your groceries is $", income, which is" , groceries_percentage, "of your income")
+print("your groceries is $", groceries, "which is" , groceries_percentage, "of your income")
 # Your savings is $XX.XX which is XX% of your income. (print)
-print()
+print("your transportation is $", transportation, "which is" , transportation_percentage, "of your income") 
 # Your spendings is $XX.XX which is XX% of your income. (print)
-
-# Your spending is $XX.XX which is XX% of your income. (print)
+print("your spending is $", spending, "which is" , spending_percentage, "of your income")
+# Your savings is $XX.XX which is XX% of your income. (print)
+print("your saving is $", saving, "which is" , saving_percentage, "of your income")
