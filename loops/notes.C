9c52df43d9cac4225ea0dc4a788fe62f5213643a
LilBrahm21 <brahm.brar@ucas-edu.net>
2025-03-05 11:48:44 -0700

    loops_notes.C

---
+++ loops/notes.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+
+    int start = 0
+    while(start <5){
+        print("hello!\n");
+        star++;
+    }
+    int q;
+    for (q=0;q<5;q++){
+        print("%d\n",q);
+    }
+    printf("hello world");
+  
+int grades[] = {88, 98,100, 86,90,99,54};
+
+printf("CSP Grade: %d\n", grades[2]);
+
+grades[2] = 95;
+printf("CSP Grade: %d\n", grades[2])
+
+int size = sizeof (grades);
+printf("%d\n", size)
+  return 0;
+}
