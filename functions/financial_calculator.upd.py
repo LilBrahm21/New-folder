492fbdff1eabae7016c6f2f707eabcdde34c5223
LilBrahm21 <brahm.brar@ucas-edu.net>
2025-03-20 11:49:18 -0600

    financialcalculator.py

--- functions/financial_calculator.upd.py
+++ functions/financial_calculator.upd.py
@@ -1,29 +1,28 @@
 #Brahm Brar Fiancial Calculator Python
-def info(cost,income,type):
-    percentage= cost/income*100
-    print(f"your {type} is ${cost:.2f} rent which is,{percentage}%, of your income")
-          
-# print statement that welcomes user and tells what program does
+print("Welcomes user this is a fanacial calculator for your needs.")
 
-# ask what their income is(varible an input)
-income= float(input("what is your inocme?\n"))
-# ask what their rent is(varible an input)
-rent= float(input("what is your rent?\n"))
-# ask what their utilites is(varible an input)
-utilities= float(input("what is your utilites?\n"))
-# ask what their groceries is(varible an input)
-groceries= float(input("what is your groceries?\n"))
-# ask what their transportation is(varible an input)
-transportation= float(input("what is your transportation?\n"))
-# calculate savings as 10% of income (income*.1) (variable)
-saving= income*.1
-# calculate spending as income-savings-rent-utilites-groceries-transportation (variable)
-spending = income-rent-utilities-groceries-transportation-saving
+def values(value):
+    float(input(f"What is your {value}?\n"))
 
-info(rent,income, "rent")
-info(utilities,income, "utilites")
-info(groceries,income, "groceries")
-info(transportation,income, "transportation")
-info(rent,income, "rent")
-info(saving,income, "saving")
-info(spending,income, "spending")
+income = values("income")
+rent = values("rent")
+utlities = values("utlities")
+groceries = values("groceries")
+transportation = values("transportation")
+
+# calculate savings as 10% of income (income*.1) (varible)
+savings = (income*.1)
+saving = (savings/income)*100
+spending = income-savings-rent-utlities-groceries-transportation
+spendings =(spending/income)*100
+
+def info(cost, income, type):
+    percent = (cost/income)*100
+    print(f"your {type} is ${cost:.2f}, Which is, {percent}, of your income. \n")
+
+info(rent, income, "rent")
+info(utlities, income, "utities")
+info(groceries, income, "groceries")
+info(transportation, income, "transportation")
+info(saving, income, "saving")
+info(spending, income, "spending")
